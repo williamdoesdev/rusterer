80ec4593c2a3a7eb95b906e639b473d55f5414b1
William <william@wikiplantia.com>
2023-05-20 11:09:13 -0500

    I think I'm going with glow

--- src/main.rs
+++ src/main.rs
@@ -1,168 +1,148 @@
-extern crate glfw;
-extern crate gl;
-
-use std::mem::*;
-use gl::types::*;
-use std::os::raw::c_void;
-use glfw::{Action, Context, Key};
-
-fn compile_shader(shader_type: GLenum, source: &str) -> u32 {
-    unsafe {
-        let shader = gl::CreateShader(shader_type); // create a shader
-        // let c_str = std::ffi::CString::new(source).unwrap();
-        // let c_str_ptr = c_str.as_ptr();
-        let source_ptr = source.as_ptr() as *const i8;
-        gl::ShaderSource(
-            shader, // shader to compile
-            1, // number of strings to compile
-            &source_ptr, // pointer to the array of pointers, or a pointer to a pointer in this case
-            &(source.len() as i32) // length of the strings, null means that the strings are null-terminated
-        ); 
-        gl::CompileShader(shader); // compile the shader
-
-        // Check for errors
-        let mut success: GLint = 1;
-        gl::GetShaderiv(shader, gl::COMPILE_STATUS, &mut success);
-        if success == 0 {
-            let mut len: GLint = 0;
-            gl::GetShaderiv(shader, gl::INFO_LOG_LENGTH, &mut len);
-            let error = std::ffi::CString::new(std::iter::repeat(' ').take(len as usize).collect::<String>()).unwrap();
-            gl::GetShaderInfoLog(shader, len, std::ptr::null_mut(), error.as_ptr() as *mut GLchar);
-            println!("Shader compilation failed: {}", error.to_str().unwrap());
-            gl::DeleteShader(shader);
-            return 0;
-        }
-
-        return shader
-    }
-}
-
-fn create_shader(vertex_shader: &str, fragment_shader: &str) -> u32 {
-    unsafe {
-        // Create a program and attach the shaders
-        let program = gl::CreateProgram();
-        let vs = compile_shader(gl::VERTEX_SHADER, vertex_shader);
-        let fs = compile_shader(gl::FRAGMENT_SHADER, fragment_shader);
-
-        gl::AttachShader(program, vs);
-        gl::AttachShader(program, fs);
-        gl::LinkProgram(program);
-        gl::ValidateProgram(program);
-
-        gl::DeleteShader(vs);
-        gl::DeleteShader(fs);
-
-        return program;
-    }
-}
+use glow::*;
 
 fn main() {
-    // Initialize the glfw library
-    let mut glfw_context = glfw::init(glfw::FAIL_ON_ERRORS).expect("Could not initialize glfw");
-
-    println!("GLFW version: {}", glfw::get_version_string());
-
-    // Create a windowed mode window and its OpenGL context
-    let (mut window, events) = glfw_context.create_window(300, 300, "Rusterer", glfw::WindowMode::Windowed)
-        .expect("Failed to create GLFW window.");
-
-    // Make the window's context current
-    window.make_current();
-    window.set_key_polling(true);
-
-    // Load OpenGL function pointers
-    gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
-
-
-
-    // Vertex buffer
-    let positions = [
-    -0.5, -0.5, 0.0, 1.0,
-     0.0,  0.5, 0.0, 1.0,
-     0.5, -0.5, 0.0, 1.0,
-    ];
-
-    let mut vertex_buffer: u32 = 0;
     unsafe {
-        gl::GenBuffers(1, &mut vertex_buffer); // generate a buffer
-        gl::BindBuffer(gl::ARRAY_BUFFER, vertex_buffer); // bind the buffer
-        gl::BufferData(
-            gl::ARRAY_BUFFER, // type of buffer
-            (positions.len() * size_of::<f32>()) as GLsizeiptr, // size in bytes
-            positions.as_ptr() as *const c_void, // data pointer
-            gl::STATIC_DRAW // mode
-        );
+        // Create a context from a sdl2 window
+        let (gl, window, mut events_loop, _context) = create_sdl2_context();
 
-        gl::VertexAttribPointer(
-            0, // index
-            4, // size (number of components)
-            gl::FLOAT, // type
-            gl::FALSE, // whether or not to normalize the data
-            (size_of::<f32>() * 4) as i32, // stride between vertices
-            std::ptr::null() // Offset of the attribute in the vertex
-        );
-        gl::EnableVertexAttribArray(0);
-    }
+        // Create a shader program from source
+        let program = create_program(&gl, VERTEX_SHADER_SOURCE, FRAGMENT_SHADER_SOURCE);
+        gl.use_program(Some(program));
 
+        // Create a vertex buffer and vertex array object
+        let (vbo, vao) = create_vertex_buffer(&gl);
 
+        // Upload some uniforms
+        set_uniform(&gl, program, "blue", 0.8);
 
-    // Shader
-    let vertex_shader = r#"
-        #version 330 core
+        gl.clear_color(0.1, 0.2, 0.3, 1.0);
 
-        layout(location = 0) in vec4 position;
-
-        void main() {
-            gl_Position = position;
-        }
-        "#;
-
-    let fragment_shader = r#"
-        #version 330 core
-
-        layout(location = 0) out vec4 color;
-
-        void main() {
-            color = vec4(1.0, 0.0, 0.0, 1.0);
-        }
-        "#;
-
-    let shader = create_shader(vertex_shader, fragment_shader);
-    unsafe {
-        gl::UseProgram(shader);
-    }
-    
-    // Loop until the user closes the window
-    while !window.should_close() {
-        // Set viewport size
-        unsafe{
-            let (width, height) = window.get_size();
-            gl::Viewport(0, 0, width, height);
-        }
-        // Clear the screen
-        unsafe {
-            gl::ClearColor(0.0, 0.0, 0.0, 1.0);
-            gl::Clear(gl::COLOR_BUFFER_BIT);
-        }
-        
-        //Render happens here
-        unsafe {
-            gl::DrawArrays(gl::TRIANGLES, 0, 3);
-        }
-
-        // Swap front and back buffers
-        window.swap_buffers();
-
-        // Poll for and process events
-        glfw_context.poll_events();
-        for (_, event) in glfw::flush_messages(&events) {
-            println!("{:?}", event);
-            match event {
-                glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
-                    window.set_should_close(true)
-                },
-                _ => {},
+        'render: loop {
+            {
+                for event in events_loop.poll_iter() {
+                    if let sdl2::event::Event::Quit { .. } = event {
+                        break 'render;
+                    }
+                }
             }
+
+            gl.clear(glow::COLOR_BUFFER_BIT);
+            gl.draw_arrays(glow::TRIANGLES, 0, 3);
+            window.gl_swap_window();
         }
+
+        // Clean up
+        gl.delete_program(program);
+        gl.delete_vertex_array(vao);
+        gl.delete_buffer(vbo)
     }
 }
+
+unsafe fn create_sdl2_context() -> (
+    glow::Context,
+    sdl2::video::Window,
+    sdl2::EventPump,
+    sdl2::video::GLContext,
+) {
+    let sdl = sdl2::init().unwrap();
+    let video = sdl.video().unwrap();
+    let gl_attr = video.gl_attr();
+    gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
+    gl_attr.set_context_version(3, 3);
+    gl_attr.set_context_flags().forward_compatible().set();
+    let window = video
+        .window("Hello triangle!", 1024, 769)
+        .opengl()
+        .resizable()
+        .build()
+        .unwrap();
+    let gl_context = window.gl_create_context().unwrap();
+    let gl = glow::Context::from_loader_function(|s| video.gl_get_proc_address(s) as *const _);
+    let event_loop = sdl.event_pump().unwrap();
+
+    (gl, window, event_loop, gl_context)
+}
+
+unsafe fn create_program(
+    gl: &glow::Context,
+    vertex_shader_source: &str,
+    fragment_shader_source: &str,
+) -> NativeProgram {
+    let program = gl.create_program().expect("Cannot create program");
+
+    let shader_sources = [
+        (glow::VERTEX_SHADER, vertex_shader_source),
+        (glow::FRAGMENT_SHADER, fragment_shader_source),
+    ];
+
+    let mut shaders = Vec::with_capacity(shader_sources.len());
+
+    for (shader_type, shader_source) in shader_sources.iter() {
+        let shader = gl
+            .create_shader(*shader_type)
+            .expect("Cannot create shader");
+        gl.shader_source(shader, shader_source);
+        gl.compile_shader(shader);
+        if !gl.get_shader_compile_status(shader) {
+            panic!("{}", gl.get_shader_info_log(shader));
+        }
+        gl.attach_shader(program, shader);
+        shaders.push(shader);
+    }
+
+    gl.link_program(program);
+    if !gl.get_program_link_status(program) {
+        panic!("{}", gl.get_program_info_log(program));
+    }
+
+    for shader in shaders {
+        gl.detach_shader(program, shader);
+        gl.delete_shader(shader);
+    }
+
+    program
+}
+
+unsafe fn create_vertex_buffer(gl: &glow::Context) -> (NativeBuffer, NativeVertexArray) {
+    // This is a flat array of f32s that are to be interpreted as vec2s.
+    let triangle_vertices = [0.5f32, 1.0f32, 0.0f32, 0.0f32, 1.0f32, 0.0f32];
+    let triangle_vertices_u8: &[u8] = core::slice::from_raw_parts(
+        triangle_vertices.as_ptr() as *const u8,
+        triangle_vertices.len() * core::mem::size_of::<f32>(),
+    );
+
+    // We construct a buffer and upload the data
+    let vbo = gl.create_buffer().unwrap();
+    gl.bind_buffer(glow::ARRAY_BUFFER, Some(vbo));
+    gl.buffer_data_u8_slice(glow::ARRAY_BUFFER, triangle_vertices_u8, glow::STATIC_DRAW);
+
+    // We now construct a vertex array to describe the format of the input buffer
+    let vao = gl.create_vertex_array().unwrap();
+    gl.bind_vertex_array(Some(vao));
+    gl.enable_vertex_attrib_array(0);
+    gl.vertex_attrib_pointer_f32(0, 2, glow::FLOAT, false, 8, 0);
+
+    (vbo, vao)
+}
+
+unsafe fn set_uniform(gl: &glow::Context, program: NativeProgram, name: &str, value: f32) {
+    let uniform_location = gl.get_uniform_location(program, name);
+    // See also `uniform_n_i32`, `uniform_n_u32`, `uniform_matrix_4_f32_slice` etc.
+    gl.uniform_1_f32(uniform_location.as_ref(), value)
+}
+
+const VERTEX_SHADER_SOURCE: &str = r#"#version 330
+  in vec2 in_position;
+  out vec2 position;
+  void main() {
+    position = in_position;
+    gl_Position = vec4(in_position - 0.5, 0.0, 1.0);
+  }"#;
+const FRAGMENT_SHADER_SOURCE: &str = r#"#version 330
+  precision mediump float;
+  in vec2 position;
+  out vec4 color;
+  uniform float blue;
+  void main() {
+    color = vec4(position, blue, 1.0);
+  }"#;
