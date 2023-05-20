09a7642d8492a0405325f388c2e3e6019b852d49
William <william@wikiplantia.com>
2023-05-20 11:16:24 -0500

    split out context module

---
+++ src/context.rs
@@ -0,0 +1,26 @@
+use glow::*;
+
+pub unsafe fn create_sdl2_context() -> (
+    glow::Context,
+    sdl2::video::Window,
+    sdl2::EventPump,
+    sdl2::video::GLContext,
+) {
+    let sdl = sdl2::init().unwrap();
+    let video = sdl.video().unwrap();
+    let gl_attr = video.gl_attr();
+    gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
+    gl_attr.set_context_version(3, 3);
+    gl_attr.set_context_flags().forward_compatible().set();
+    let window = video
+        .window("Hello triangle!", 1024, 769)
+        .opengl()
+        .resizable()
+        .build()
+        .unwrap();
+    let gl_context = window.gl_create_context().unwrap();
+    let gl = glow::Context::from_loader_function(|s| video.gl_get_proc_address(s) as *const _);
+    let event_loop = sdl.event_pump().unwrap();
+
+    (gl, window, event_loop, gl_context)
+}
